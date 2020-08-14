8e2c3e41df9824b56dad6560e2e548256df8b5ba
Gordon Preumont <preumontgr@gmail.com>
2020-08-14 17:15:56 +0200

    reset

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        message: 'Hello World',
-        link: 'https://www.google.be',
-        success: true,
-        persons: ["Jim", "Paul", "Elena", "Kate"],
-        style: { fontWeight: 'bold' },
-        className: 'checked',
+
     },
     methods: {
-        switchMe: function() {
-            this.success = !this.success
-        },
-        styleSwitch: function() {
-            if(this.success) {
-                return { 
-                    background: 'green',
-                    color: 'white',
-                }
-            }
-            return { 
-                background: 'red',
-                color: 'white',
-            }
-        }
+        
     }
 })
