71b3a777996ed1b0a68aa1990d8b123bb2d246e7
Gordon Preumont <preumontgr@gmail.com>
2020-08-14 18:31:03 +0200

    getter & setter

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ let vm = new Vue({
     data: {
         success: false,
         message: "",
+        firstname: 'Gordon',
+        lastname: 'Preumont'
     },
     computed: {
         cls: function() {
             console.log('cls called')
             return this.success ? 'success' : 'error'
+        },
+        fullname: {
+            get: function() {
+                return `${this.firstname} ${this.lastname}`
+            },
+            set: function(value) {
+                const nameParts = value.split(' ')
+                this.firstname = nameParts[0]
+                this.lastname = nameParts[1]
+            }
         }
     }
 })
