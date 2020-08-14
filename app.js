0425de658b74187003f37487630afce66e5fb7e8
Gordon Preumont <preumontgr@gmail.com>
2020-08-14 17:01:09 +0200

    mounted

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 let vm = new Vue({
     el: '#app',
     data: {
-
+        seconds: 0
     },
     methods: {
-
-    }
+        
+    },
+    mounted() {
+        setInterval(() => {
+            this.seconds++
+        }, 1000)
+    },
 })
