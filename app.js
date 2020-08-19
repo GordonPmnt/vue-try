ea9a25be901c783c1568232c9df33afce0475a3f
Gordon Preumont <preumontgr@gmail.com>
2020-08-19 17:54:43 +0200

    click @ model types

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 let vm = new Vue({
     el: '#app',
     data: {
-
+        link: "https://www.google.be",
+        normal: "normal",
+        lazy: "lazy",
+        keyboard: "keyboard",
     },
     methods: {
-
+        clicked() {
+            console.log("click")
+        },
+        pressed() {
+            alert("you pressed SPACE")
+        },
+        clickOne() {
+            alert(1)
+        },
+        clickTwo() {
+            alert(2)
+        }
+    }
 })
