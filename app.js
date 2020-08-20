75faa1617ebd371b409841be3633482fda68a589
Gordon Preumont <preumontgr@gmail.com>
2020-08-20 16:44:30 +0200

    store

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
-let vm = new Vue({
+class NotificationsStore {
+    state = {
+        count: 0
+    }
+    increment() {
+        this.state.count++
+    }
+    decrement() {
+        this.state.count--
+    }
+}
+
+var notifications_store = new NotificationsStore()
+
+var Counter = {
+    data() {
+        return { 
+            state: notifications_store.state 
+        }
+    },
+    template: `
+        <div>
+            <p>{{ state.count }}</p>
+        </div>
+    `
+};
+
+var Notifications = {
+    methods: {
+        addNotification() {
+            notifications_store.increment()
+        }
+    },
+    components: { Counter },
+    template: `
+        <div>
+            <button @click="addNotification">Add</button>
+            <h1>Counter:</h1>
+            <counter></counter>
+        </div>
+    `,
+};
+
+new Vue({
     el: '#app',
+    components: { Notifications, Counter },
     data: {
 
     },
-    methods: {
-
-    }
 })
