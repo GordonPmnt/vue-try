a3cc6b954378a63d74e83701f5adea2161829984
Gordon Preumont <preumontgr@gmail.com>
2020-08-20 12:40:55 +0200

    basic components

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
+let message = {
+    props: ['message', 'other'],
+    template: `
+        <p>Message: {{ message }} - {{ other }}</p>
+    `,
+}
+
+let newmessage = {
+    props: {
+        message: String,
+        comment: { type: String, default: 'Default comment' },
+        etc: [String, Number]
+    },
+    template: `
+        <p>Message: {{ message }} - {{ comment }}</p>
+    `,
+}
+
+let counter = {
+    props: {
+        start: { type: Number, default: 0 }
+    },
+    template: `
+        <div>
+            <p>{{ total }}</p>
+            <button @click="increment" >+1</button>
+        </div>
+    `,
+    data() {
+        return {
+            num: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.num++
+        }
+    },
+    computed: { 
+        total() {
+            return this.start + this.num
+        }
+    }
+}
+
 let vm = new Vue({
     el: '#app',
+    components: { message, newmessage, counter },
     data: {
-
+        message: "Hey there!"
     },
     methods: {
 
