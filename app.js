b8a635392098efad1fe4bca1c42384dc3fc84c44
Gordon Preumont <preumontgr@gmail.com>
2020-08-20 11:19:08 +0200

    filters

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//global filters
+Vue.filter('capitalize', value => value.toUpperCase())
+Vue.filter('reverse', value => value.split('').reverse().join(''))
+
+var addstring = (value, param) => value + param;
+
 let vm = new Vue({
     el: '#app',
     data: {
-
+        message: "Hey there!"
     },
-    methods: {
-
+    // local filters
+    filters: {
+        addstring
     }
 })
